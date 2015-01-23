772ba4678da581918de7cb7178eb9cd47006417a
Sam Gleske <sam.mxracer@gmail.com>
2015-01-23 01:02:40 -0500

    Convert options from OpenStruct to Hash
    This sets us up to load overrides from a config file and merge the config file
    into the options overriding arguments that were passed in.

--- repository-secrets.rb
+++ repository-secrets.rb
@@ -16,7 +16,6 @@ require 'openssl'
 require 'base64'
 #required for option parsing
 require 'optparse'
-require 'ostruct'
 #pretty printing of ruby objects
 require 'pp'
 #for file copying
@@ -39,13 +38,13 @@ class OptparseExample
   def self.parse(args)
     # The options specified on the command line will be collected in *options*.
     # We set default values here.
-    options = OpenStruct.new
-    options.private_key_file = "secrets/id_rsa"
-    options.public_key_file = "secrets/id_rsa.pub"
-    options.secret_text_tag = "supersecret"
-    options.verbose = 0
-    options.decrypt = false
-    options.files = []
+    options = Hash.new
+    options["private_key_file"] = "secrets/id_rsa"
+    options["public_key_file"] = "secrets/id_rsa.pub"
+    options["secret_text_tag"] = "supersecret"
+    options["verbose"] = 0
+    options["decrypt"] = false
+    options["files"] = []
     opt_parser = OptionParser.new do |opts|
       opts.banner = "Usage: repository-secrets.rb [options] [arguments]"
 
@@ -69,7 +68,7 @@ class OptparseExample
 
       opts.on("--public-key FILE",
               "Path to a public key to use for encryption.") do |file|
-        options.public_key_file = file
+        options["public_key_file"] = file
       end
 
       #Decrypt mode options
@@ -81,14 +80,14 @@ class OptparseExample
       opts.on_tail("-d", "--decrypt",
                    "Force Decrypt mode to be on.  Not really",
                    "necessary.") do
-        options.decrypt = true
+        options["decrypt"] = true
       end
 
       opts.on("--private-key FILE",
               "Path to a private key to use for",
               "decryption.") do |file|
-        options.private_key_file = file
-        options.decrypt = true
+        options["private_key_file"] = file
+        options["decrypt"] = true
       end
 
       # Mandatory argument.
@@ -99,15 +98,15 @@ class OptparseExample
               "list replacing secrets with the decrypted",
               "text.  File paths are either full path or",
               "relative to the current working directory.") do |file|
-        options.secrets_file = file
-        options.decrypt = true
+        options["secrets_file"] = file
+        options["decrypt"] = true
       end
 
       opts.on("-f", "--file FILE",
               "Use string interpolation on the specified",
               "FILE to decrypt secrets.") do |file|
-        options.files << file
-        options.decrypt = true
+        options["files"] << file
+        options["decrypt"] = true
       end
 
       # Optional argument; multi-line description.
@@ -116,12 +115,12 @@ class OptparseExample
               "in-place editing.  Otherwise print",
               "decrypted file to stdout.",
               "  (make backup if EXTENSION supplied)") do |ext|
-        options.inplace = true
-        options.extension = ext
-        if options.extension
-          options.extension.sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
+        options["inplace"] = true
+        options["extension"] = ext
+        if options["extension"]
+          options["extension"].sub!(/\A\.?(?=.)/, ".")  # Ensure extension begins with dot.
         end
-        options.decrypt = true
+        options["decrypt"] = true
       end
 
       opts.separator ""
@@ -133,7 +132,7 @@ class OptparseExample
               "Change the unique text which defines the",
               "tag to be interpolated in files.  By",
               "default: supersecret") do |tag|
-        options.secret_text_tag = tag
+        options["secret_text_tag"] = tag
       end
 
       opts.on("-v", "--[no-]verbose",
@@ -142,7 +141,7 @@ class OptparseExample
               "  WARNING: -vvv displays plain text secrets") do |v|
         #increment verbosity by 1 for each -v used
         if v
-          options.verbose += 1
+          options["verbose"] += 1
         end
       end
 
@@ -171,7 +170,7 @@ end  # class OptparseExample
 
 #print to stderr with a verbosity level
 def verbose(level, text)
-  if $options.verbose >= level
+  if $options["verbose"] >= level
     $stderr.puts "v#{level}> #{text}".gsub(/^v0> /, "")
   end
 end # verbose()
@@ -179,7 +178,7 @@ end # verbose()
 #First decodes base64 cipher text input and then decrypts; returns plain text
 def decrypt(ciphertext)
   verbose 3, "decrypt(\"#{ciphertext}\")"
-  private_key=OpenSSL::PKey::RSA.new(File.read($options.private_key_file))
+  private_key=OpenSSL::PKey::RSA.new(File.read($options["private_key_file"]))
   plaintext=private_key.private_decrypt(Base64.strict_decode64(ciphertext))
   verbose 3, "  returns \"#{plaintext}\""
   return plaintext
@@ -188,7 +187,7 @@ end # decrypt()
 #First encrypts plain text input and then encodes in base64; returns cipher text
 def encrypt(plaintext)
   verbose 3, "encrypt(\"#{plaintext}\")"
-  public_key = OpenSSL::PKey::RSA.new(File.read($options.public_key_file))
+  public_key = OpenSSL::PKey::RSA.new(File.read($options["public_key_file"]))
   ciphertext = Base64.strict_encode64(public_key.public_encrypt(plaintext))
   verbose 3, "  returns \"#{ciphertext}\""
   return ciphertext
@@ -207,39 +206,39 @@ verbose 2, "Arguments data structures (parsed and passed)"
 verbose 2, PP.pp($options, "")
 verbose 2, PP.pp(ARGV, "")
 
-if (ARGV.length > 0) || $options.decrypt
-  if $options.decrypt
+if (ARGV.length > 0) || $options["decrypt"]
+  if $options["decrypt"]
     verbose 0, "Performing decryption with string interpolation on files."
-    if $options.inplace
+    if $options["inplace"]
       verbose 0, "Inplace editing enabled for all files."
     end
     #read the secrets file
-    if $options.secrets_file
+    if $options["secrets_file"]
       verbose 0, "Reading a secrets file for a list of files."
-      f = File.open($options.secrets_file, "r")
+      f = File.open($options["secrets_file"], "r")
       f.each_line do |line|
         #skip blank lines
         next if line.strip.length == 0
         #skip lines that start with a hash
         next if /^\s*#.*/.match(line.strip)
-        $options.files << line.strip
+        $options["files"] << line.strip
       end
       f.close
     end
-    $options.files.each do |file|
+    $options["files"].each do |file|
       verbose 0, "Performing string interpolation on: #{file}"
       filecontents = File.read(file)
-      secrets = filecontents.scan(/\${#{Regexp.escape($options.secret_text_tag)}:[^}]*}/)
+      secrets = filecontents.scan(/\${#{Regexp.escape($options["secret_text_tag"])}:[^}]*}/)
       secrets.each do |secret|
         #extract just the cipher text from the secret
-        ciphertext=secret.gsub(/\${#{Regexp.escape($options.secret_text_tag)}:([^}]*)}/,'\1')
+        ciphertext=secret.gsub(/\${#{Regexp.escape($options["secret_text_tag"])}:([^}]*)}/,'\1')
         #inline string replace the secret with the plain text
         filecontents.gsub!(secret,decrypt(ciphertext))
       end
-      if $options.inplace
-        if $options.extension
-          FileUtils.cp(file, file+$options.extension)
-          verbose 1, "Making a backup of #{file} to #{file+$options.extension}"
+      if $options["inplace"]
+        if $options["extension"]
+          FileUtils.cp(file, file+$options["extension"])
+          verbose 1, "Making a backup of #{file} to #{file+$options["extension"]}"
         end
         verbose 1, "Writing to #{file}"
         f = File.open(file,"w")
@@ -251,23 +250,23 @@ if (ARGV.length > 0) || $options.decrypt
     end
   else
     ARGV.each do |plaintext|
-      puts "${#{$options.secret_text_tag}:#{encrypt(plaintext.strip)}}"
+      puts "${#{$options["secret_text_tag"]}:#{encrypt(plaintext.strip)}}"
     end
   end
 else
   verbose 0, "Reading from stdin.  Use CTRL+D to finish."
   ARGF.each do |plaintext|
-    if $options.decrypt
+    if $options["decrypt"]
       verbose 0, "ERR: You may only decrypt in normal mode not using stdin."
       exit 1
     end
     next if plaintext.strip.length == 0
-    puts "${#{$options.secret_text_tag}:#{encrypt(plaintext.strip)}}"
+    puts "${#{$options["secret_text_tag"]}:#{encrypt(plaintext.strip)}}"
   end
 end
 
 #output info message to stderr
-if not $options.decrypt
+if not $options["decrypt"]
   verbose 0, ""
   verbose 0, "Replace the plain text string in your config file with the secure one."
   verbose 0, "NOTE: Don't forget to regenerate passwords or API keys if you've already"
